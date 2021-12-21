d07e1fddf2be2afc87bf9c7147f6cb2285e26b73
HemantDhavle <96099050+HemantDhavle@users.noreply.github.com>
2021-12-21 16:14:48 +0530

    Update loginTest.java

--- src/test/java/com/opencart/qa/tests/loginTest.java
+++ src/test/java/com/opencart/qa/tests/loginTest.java
@@ -3,7 +3,7 @@ package com.opencart.qa.tests;
 public class loginTest {
 public void loginsTest() {
 	System.out.println("asd");
-
+System.out.println("suresh");
 	System.out.println("hemant");
 
 }
