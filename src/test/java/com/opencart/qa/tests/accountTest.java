5e2745e2c50c98b7303b50585b24127e6d08608e
HemantDhavle <96099050+HemantDhavle@users.noreply.github.com>
2021-12-21 16:52:29 +0530

    Create accountTest.java

---
+++ src/test/java/com/opencart/qa/tests/accountTest.java
@@ -0,0 +1,16 @@
+package com.opencart.qa.tests;
+
+public class accountTest 
+
+{
+	public void testAccount()
+	{
+		int a =10;
+		int b =20;
+		int c = a+b;
+    int d = c;
+		System.out.println(c);
+    System.out.println(d);
+	}
+
+}
